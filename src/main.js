b126535d7f3052f80385a02bd61fdf8942eff8d5
ushaHernandez <ursunina@hotmail.com>
2019-07-09 14:10:09 -0500

    Funcion main

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const data = RICKANDMORTY;
+console.log(data);
+const card = document.getElementById ("rick-mory-card")
+
+
+const dataSee = (data)=>{
+  let str = ''
+    data.results.forEach(element => {
+      console.log(element )
+     str +=`<div class="card"><p>${element.name}<p>
+     <p>${element.status}<p>
+     <p> specie: ${element.species}<p> <p>${element.gender}<p><p>${element.origin.name}<p><p>${element.location.name}<p>
+     <img src = "${element.image}"> </di>`
+
+     
+   });
+   card.innerHTML = str
+}
+dataSee(data)
