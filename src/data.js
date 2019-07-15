a36ebb2706ff3a74ece77426b139c443598b4006
ushaHernandez <ursunina@hotmail.com>
2019-07-15 14:13:28 -0500

    actualizando todo

--- src/data.js
+++ src/data.js
@@ -1,22 +1,45 @@
-
-const data = RICKANDMORTY;
-console.log(data);
-const card = document.getElementById ("rick-mory-card")
-
-
-
-const dataSee = (data)=>{
-  let str = ''
-    data.results.forEach(element => {
-      console.log(element )
-     str +=`<div class="card"><p>${element.name}<p>
-     <p>${element.status}<p>
-     <p> specie: ${element.species}<p> <p>${element.gender}<p><p>${element.origin.name}<p><p>${element.location.name}<p>
-     <img src = "${element.image}"> </di>`
-
+window.dataManager = {
+   
+    filterRick : (data) => {
+        let items = data.filter(element => element.name.indexOf('Rick') > -1);
+        console.log(items)
+        return items;
+    },
+    filterMorty :(data) => {
+        let items = data.filter(element => element.name.indexOf('Morty') > -1);
+        return items;
+    },
+    
+    filterOtros : (data) => {
+    let items = data.filter(element => element.name.includes('Rick') == false && element.name.includes('Morty') == false  );
+    console.log(items) 
+    return items;
+    
+  },
+    
+    orderAz:(data,az)=>{
+    if (az == "A-Z"){
+      const result= data.sort((a,b)=>{
+          if (a.name.toLowerCase() < b.name.toLowerCase()
+          ) return -1;
+          if (a.name.toLowerCase() > b.name.toLowerCase()
+          ) return 1;
+          return 0;
+      });
+      return result;
+  }
+  else {
+      const result= data.sort((a,b)=>{
+          if (a.name.toLowerCase() > b.name.toLowerCase()
+          ) return -1;
+          if (a.name.toLowerCase() < b.name.toLowerCase()
+          ) return 1;
+          return 0;
+      });
+      return result;
+  }
+  }
      
-   });
-   card.innerHTML = str
-}
-dataSee(data)
-
+​}
+    // window.order = order;
+    // window.orderZA = orderZA;
--- src/main.js
+++ src/main.js
@@ -1,19 +1,95 @@
-const data = RICKANDMORTY;
-console.log(data);
-const card = document.getElementById ("rick-mory-card")
+const data = RICKANDMORTY.results;
+const card = document.getElementById("rick-mory-card");
+const buttonRicks = document.getElementById("button-ricks");
+const buttonMorty = document.getElementById("button-morty");
+const buttonOtros = document.getElementById("button-otros");
+const odrenar = document.getElementById("ordenar");
 
 
-const dataSee = (data)=>{
-  let str = ''
-    data.results.forEach(element => {
-      console.log(element )
-     str +=`<div class="card"><p>${element.name}<p>
-     <p>${element.status}<p>
-     <p> specie: ${element.species}<p> <p>${element.gender}<p><p>${element.origin.name}<p><p>${element.location.name}<p>
-     <img src = "${element.image}"> </di>`
+let dataSee = (data) => {
+    let str = ''
+    data.forEach(element => {
+        console.log(element)
+        str +=`<div class="card"><p> Nombre: ${element.name}<p>
+        <p>Estado: ${element.status}<p>
+        <p> Specie: ${element.species}<p> <p>Genero: ${element.gender}<p><p>Lugar de Origen: ${element.origin.name}<p><p>Ubicacion Actual: ${element.location.name}<p>
+     <img src ="${element.image}" id="imagen"> </div>`
 
-     
-   });
-   card.innerHTML = str
+
+    });
+    card.innerHTML = str
 }
 dataSee(data)
+
+let resultRick = (data) => {
+    // let userElectionRick = buttonRicks.dataset.text;
+    // console.log(userElectionRick)
+    let result = window.dataManager.filterRick(data);
+    let str = ''
+    result.forEach(element => {
+        // console.log(element);
+        str += `<div class="card"><p> Nombre: ${element.name}<p>
+     <p>Estado: ${element.status}<p>
+     <p> Specie: ${element.species}<p> <p>Genero: ${element.gender}<p><p>Lugar de Origen: ${element.origin.name}<p><p>Ubicacion Actual: ${element.location.name}<p>
+     <img src = "${element.image}"width="80%" height="90%"> </div>`
+    });
+     
+    card.innerHTML = str;
+    
+}
+
+
+buttonRicks.addEventListener('click', () => {
+    resultRick(data)
+});
+
+let resultMorty = (data) => {
+    // let userElectionRick = buttonRicks.dataset.text;
+    // console.log(userElectionRick)
+    let result = window.dataManager.filterMorty(data);
+    let str = ''
+    result.forEach(element => {
+        // console.log(element);
+        str += `<div class="card"><p> Nombre: ${element.name}<p>
+     <p>Estado: ${element.status}<p>
+     <p> Specie: ${element.species}<p> <p>Genero: ${element.gender}<p><p>Lugar de Origen: ${element.origin.name}<p><p>Ubicacion Actual: ${element.location.name}<p>
+     <img src = "${element.image}"width="80%" height="90%"> </div>`
+    });
+     
+    card.innerHTML = str;
+    
+}
+
+
+buttonMorty.addEventListener('click', () => {
+    resultMorty(data)
+});
+
+let resultOtros = (data) => {
+    // let userElectionRick = buttonRicks.dataset.text;
+    // console.log(userElectionRick)
+    let result = window.dataManager.filterOtros(data);
+    let str = ''
+    result.forEach(element => {
+        // console.log(element);
+        str += `<div class="card"><p> Nombre: ${element.name}<p>
+     <p>Estado: ${element.status}<p>
+     <p> Specie: ${element.species}<p> <p>Genero: ${element.gender}<p><p>Lugar de Origen: ${element.origin.name}<p><p>Ubicacion Actual: ${element.location.name}<p>
+     <img src = "${element.image}"width="80%" height="90%"> </div>`
+    });
+     
+    card.innerHTML = str;
+    
+}
+
+
+buttonOtros.addEventListener('click', () => {
+    resultOtros(data)
+});
+
+odrenar.addEventListener("change", () => {
+    let az =odrenar.value;
+    let orderResult = window.dataManager.orderAz(data,az);
+    dataSee(orderResult);
+    }
+    )
